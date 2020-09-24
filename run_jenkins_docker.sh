999a784c49092bb5d7f8769be1b7a3f765c9da0b
johanneshiry <johannes.hiry@tu-dortmund.de>
2020-09-24 11:12:02 +0200

    Update run_jenkins_docker.sh

--- run_jenkins_docker.sh
+++ run_jenkins_docker.sh
@@ -1,2 +1,2 @@
+#!/bin/bash
 docker container run --name jenkins-lts-jdk11 --rm --detach -v jenkins-data:/var/jenkins_home -v jenkins-docker-certs:/certs/client:ro -v /var/run/docker.sock:/var/run/docker.sock -p 127.0.0.1:8085:8080 -p 127.0.0.1:50000:50000 jenkins:2.249.1-lts-jdk11-jdk8 --prefix=/ci
-
